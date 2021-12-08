7480412b1fd6d1efecb727bb8ef01d2271abc91f
j-carson <44308120+j-carson@users.noreply.github.com>
2021-12-07 19:27:51 -0800

    improve day 7

--- src/07/wip.py
+++ src/07/wip.py
@@ -8,42 +8,51 @@ TEST_1_SOLUTION = 37
 TEST_2_SOLUTION = 168
 
 
-def solve1(data):
-    data = np.array(sorted(data), dtype=int)
-
-    best = np.abs(data - data[0]).sum()
-
-    for pos in data:
-        score = np.abs(data - pos).sum()
-        if score < best:
-            best = score
-
-    return best
-
-
-def score2(crabs, location):
-    distances = np.abs(crabs - location)
-    costs = (distances + 1) * distances / 2
-    return costs.sum()
-
-
-def solve2(data):
-    data = np.array(sorted(data), dtype=int)
-    low = data[0]
-    high = data[-1]
-
-    best = score2(data, data[0])
+def solve1(crabs, counts):
+    # The best spot will be somewhere between the lowest and
+    # highest location, and we'll know we crossed the minimum
+    # when we stop getting lower scores
+    low = crabs[0]
+    high = crabs[-1]
+    best = np.abs(crabs - low).sum()
 
     for pos in range(low + 1, high):
-        score = score2(data, pos)
+        score = (np.abs(crabs - pos) * counts).sum()
         if score < best:
             best = score
+        else:
+            break
 
     return best
+
+
+def score2(crabs, counts, location):
+    # formula for sum of series of integers
+    # is the average of the endpoints times
+    # the total number of items in the series
+    distances = np.abs(crabs - location)
+    costs = (distances + 1) * distances / 2
+    return (costs * counts).sum()
+
+
+def solve2(crabs, counts):
+    low = crabs[0]
+    high = crabs[-1]
+
+    best = score2(crabs, counts, low)
+
+    for pos in range(low + 1, high):
+        score = score2(crabs, counts, pos)
+        if score < best:
+            best = score
+        else:
+            break
+
+    return int(best)
 
 
 def parsetext(text):
-    return [int(i) for i in text.split(",")]
+    return np.unique([int(i) for i in text.split(",")], return_counts=True)
 
 
 def mydata():
@@ -53,24 +62,24 @@ def mydata():
 def part1():
     # turn on debug print statements
     ic.enabled = True
-    result = solve1(parsetext(TEST_INPUT))
+    result = solve1(*parsetext(TEST_INPUT))
     assert result == TEST_1_SOLUTION
 
     # turn off debug print statements
     ic.enabled = False
-    return solve1(parsetext(mydata()))
+    return solve1(*parsetext(mydata()))
 
 
 def part2():
 
     # turn on debug print statements
     ic.enabled = True
-    result = solve2(parsetext(TEST_INPUT))
+    result = solve2(*parsetext(TEST_INPUT))
     assert result == TEST_2_SOLUTION
 
     # turn off debug print statements
     ic.enabled = True
-    return solve2(parsetext(mydata()))
+    return solve2(*parsetext(mydata()))
 
 
 result = part1()
